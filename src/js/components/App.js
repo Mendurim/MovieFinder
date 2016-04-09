1e0c581d77e9eaf6836ec915a1211f6d9b425993
mendurim <mendurim.r@gmail.com>
2016-04-09 23:18:00 +0200

    Added the function to create class

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -2,3 +2,12 @@ var React = require('react');
 var AppActions = require('../actions/AppActions'); 
 var AppStore = require('../stores/AppStore'); 
 
+var App = React.createClass({
+	render: function(){
+		return(
+			<div>
+			 My App
+			</div>
+		); 
+	}
+}); 
